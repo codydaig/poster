7a87c7845b067d2e79ddf2dc9998b2f438c2109e
Duncan Nevin <duncan.nevin@gmail.com>
2016-12-01 14:18:38 -0800

    General get and post working

--- server/db.js
+++ server/db.js
@@ -1,3 +1,62 @@
-var mongoClient = require('mongodb').MongoClient;
+var mysql = require('mysql');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('posts', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
-module.exports = mongoClient;
+// Verify connection...
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection to mySQL successful!');
+  })
+  .catch(function(err) {
+    console.log('Unable to connect to db: ', err);
+  });
+
+// User table...
+
+var User = sequelize.define('user', {
+  userName: {
+    type: Sequelize.STRING,
+    field: 'user_name'
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name'
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    field: 'last_name'
+  }
+});
+
+// Post table...
+
+var Post = sequelize.define('post', {
+  userPost: {
+    type: Sequelize.STRING,
+    field: 'user_post'
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    field: 'user_id'
+  }
+});
+
+Post.sync();
+User.sync();
+User.hasMany(Post);
+Post.belongsTo(User);
+
+exports.User = User;
+exports.Post = Post;
+
+
+
+
+
+
+
--- server/post-processor.js
+++ server/post-processor.js
@@ -1,64 +1,43 @@
 var express = require('express');
 var morgan = require('morgan');
 
-var mongoDB = require('./db.js');
-
-var url = 'mongodb://localhost:27017/test';// need to add database name here.
+var db = require('./db.js');
 
 module.exports = {
   // handles adding new posts, and getting the whole collection...
   general: {
     get: function(req, res) {
-      console.log('genGET')
-      // mongoDB.connect(url, function(err, db) {
-      //   if (err) {console.log('no go on that GENERAL GET')}
-      //   else {
-      //     console.log('good you are in GENERAL GET');
-      //   }
-      // });
-      res.end();
+      db.Post.findAll({include: [db.User]})
+        .then(function(post) {
+          res.json(post)
+          res.end();
+        });
     }, // get all posts
     post: function(req, res) {
-      console.log('genPOST')
-      // mongoDB.connect(url, function(err, db) {
-      //   if (err) {console.log('no go on that GENERAL GET')}
-      //   else {
-      //     console.log('good you are in GENERAL GET');
-      //   }
-      // });
-      res.end();
+      db.User.findOrCreate({where: {userName: req.body.userName}})
+        .spread(function(user, created) {
+          db.Post.create({
+            userId: user.get('id'),
+            userPost: req.body.message
+          }).then(function(message) {
+            res.status(201);
+          });
+        });
+        res.end();
     }, // create a new post
   },
   // handles requests for specific data requests or alterations...
   custom: {
     get: function(req, res) {
-      console.log('custGET')
-      // mongoDB.connect(url, function(err, db) {
-      //   if (err) {console.log('no go on that GENERAL GET')}
-      //   else {
-      //     console.log('good you are in GENERAL GET');
-      //   }
-      // });
+
       res.end();
     }, // retreive a single post by its id
     put: function(req, res) {
-      console.log('custPUT')
-      // mongoDB.connect(url, function(err, db) {
-      //   if (err) {console.log('no go on that GENERAL GET')}
-      //   else {
-      //     console.log('good you are in GENERAL GET');
-      //   }
-      // });
 
+      res.end();
     }, // update a single post by its id
     delete: function(req, res) {
-      console.log('custDELETE')
-      // mongoDB.connect(url, function(err, db) {
-      //   if (err) {console.log('no go on that GENERAL GET')}
-      //   else {
-      //     console.log('good you are in GENERAL GET');
-      //   }
-      // });
+
       res.end();
     } // delete a single post by its id
   }
